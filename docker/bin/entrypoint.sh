c506208066ce09010a84d7195782666a95935d77
NoobLk <125950957+nooblk-98@users.noreply.github.com>
2025-06-07 23:35:56 +0530

    update base image for faster loading speed

--- docker/bin/entrypoint.sh
+++ docker/bin/entrypoint.sh
@@ -4,25 +4,22 @@ set -e  # Exit on any error
 # php storage link create
 php artisan storage:link || true
 
-echo " Installing Configurations please wait ! "
+if [ ! -f /var/www/html/.env ]; then
+    echo " Installing Configurations please wait ! "
+    sleep 10
+    echo " "
+    echo "============================================================================="
+    echo " "
 
-sleep 10
+    php artisan v2board:install
 
-echo " "
-echo "============================================================================="
-echo " "
-
-# Run Laravel install command (fail loudly if it breaks)
-php artisan v2board:install
-
-echo " "
-echo " "
-echo " "
-echo "============================================================================="
-echo " "
-
-chown -R www-data:www-data /var/www/html && \
-    chmod -R 755 /var/www/html/storage /var/www/html/bootstrap/cache
+    echo " "
+    echo " "
+    echo "============================================================================="
+    echo " "
+else
+    echo "✅ .env already exists. Skipping v2board installation."
+fi
 
 # Start supervisor services (ginx, php-fpm)
 exec supervisord -c /etc/supervisord.conf
